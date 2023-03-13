24805675a5347b024e905e0ef662c682a973587e
satel33 <jurijs.haricevs@gmail.com>
2023-03-13 08:18:24 -0600

    fix UI errors in header, make it responsive

--- src/components/Button.js
+++ src/components/Button.js
@@ -9,9 +9,9 @@ export const Button = ({
 }) => {
   return (
     <button
-      className={`border-[1px] border-white px-6 py-[13px] hover:cursor-pointer font-heading text-base font-medium leading-[22px] sm:px-8 sm:py-4 sm:text-[18px] sm:leading-6 ${type === "primary"
-          ? "bg-white text-black hover:text-blue-gray hover:shadow-button"
-          : "bg-transparent text-white hover:drop-shadow-button"
+      className={`border-[1px] border-white px-6 py-[13px] hover:cursor-pointer font-heading text-base font-medium leading-[22px] sm:px-[10px] sm:py-[10px] sm:text-[18px] xxl:py-4 xxl:px-8 sm:leading-6 ${type === "primary"
+        ? "bg-white text-black hover:text-blue-gray hover:shadow-button"
+        : "bg-transparent text-white hover:drop-shadow-button"
         } ${clsName}`}
       {...props}
     >
